660d80fd72fa8ff61b7cd709c8faa8bb0e194abb
Marius Mandal <mariusmandal@gmail.com>
2014-04-22 10:50:02 +0200

    debug

--- controller/oversikt.controller.php
+++ controller/oversikt.controller.php
@@ -123,6 +123,8 @@ foreach( $videresendte_innslag as $inn ) {
 			$videresendte['film_antall']++;
 
 			$personer = $i->personer();
+			
+			var_dump( $personer );
 			foreach( $personer as $pers )
 				$unike_personer['film'][ $pers['p_id'] ] = $pers;
 
