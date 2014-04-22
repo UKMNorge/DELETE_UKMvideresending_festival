4a5439f475cc2b1b72665d89e3cd238c8774d38b
Marius Mandal <mariusmandal@gmail.com>
2014-04-22 09:53:28 +0200

    DEVELOPMENT

--- controller/layout.controller.php
+++ controller/layout.controller.php
@@ -12,7 +12,7 @@ $videresendtil->ID		= $vt->g('pl_id');
 $videresendtil->navn 	= $vt->g('pl_name');
 $videresendtil->frist 	= $vt->g('pl_deadline');
 $videresendtil->registrert = $vt->registered();
-$videresendtil->mottakelig = $vt->subscribable();
+$videresendtil->mottakelig = true;#$vt->subscribable();
 $videresendtil->pl_start	= $vt->g('pl_start');
 $videresendtil->pl_stop		= $vt->g('pl_stop');
 
