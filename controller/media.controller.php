cb0124f0d4056fef645e7ce2dee9021e2c638086
Marius Mandal <mariusmandal@gmail.com>
2014-06-05 11:29:44 +0200

    Flytt image_selected til functions

--- controller/media.controller.php
+++ controller/media.controller.php
@@ -1,6 +1,7 @@
 <?php
 require_once('UKM/innslag.class.php');
 require_once('UKM/tittel.class.php');
+require_once( PLUGIN_DIR_PATH.'functions.php' );
 
 $videresendte = $m->videresendte();
 
@@ -94,37 +95,3 @@ foreach( $videresendte as $inn ) {
 }
 
 update_option('UKMvideresending_festival_mediaOK', $media_ok );
-
-
-function image_selected( $innslag, $tittel = false, $type = 'bilde' ) {
-	$valgtBilde = new SQL("SELECT *
-						   FROM `smartukm_videresending_media` AS `media`
-						   JOIN `ukmno_wp_related` ON (`ukmno_wp_related`.`rel_id` = `media`.`rel_id`)
-						   WHERE `media`.`b_id` = '#bid'
-						   AND `m_type` = '#type'
-  						   AND (`t_id` = '0' OR `t_id` = '#tid' OR `t_id` IS NULL)",
-						  array('bid'=>$innslag->ID,'tid'=>$tittel, 'type' => $type)
-						 );
-	$bilde = $valgtBilde->run('field','rel_id');
-	
-	if( $bilde == null )
-		return 'none_selected';
-	
-	// CALC IMAGE DATA
-	$bilde = $valgtBilde->run('array');
-
-	$post_meta = unserialize( $bilde['post_meta'] );
-	if( isset( $post_meta['sizes']['thumbnail']['file'] ) ) {
-		$src = $post_meta['sizes']['thumbnail']['file'];
-	} else {
-		$src = $post_meta['file'];
-	}
-	
-	$src = 'http://'. $_SERVER['HTTP_HOST'].'/wp-content/blogs.dir/'. $bilde['blog_id'].'/files/' . $src;
-
-	
-	$vb = new stdClass();
-	$vb->ID = $bilde['rel_id'];
-	$vb->src = $src;
-	return $vb;
-}
