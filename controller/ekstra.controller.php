243224f79e2b291acd6481854a654a9faca23734
Asgeir Stavik Hustad <askhus@online.no>
2016-08-25 02:48:50 +0200

    Bruk pl_id

--- controller/ekstra.controller.php
+++ controller/ekstra.controller.php
@@ -5,7 +5,9 @@ require_once(PLUGIN_DIR_PATH . 'class/videresendingsskjema.class.php');
 // $vt er fra layout.controller.php - monstring::videresendtil(true)
 #var_dump($vt->info['fylke_id']);
 #var_dump($vt);
-$TWIG['videresendingsskjema'] = new Videresendingsskjema($vt->info['fylke_id']);
+$TWIG['videresendingsskjema'] = new Videresendingsskjema($vt->info['fylke_id'], get_option('pl_id'));
 $TWIG['f_id'] = $vt->info['fylke_id'];
 $TWIG['pl_id'] = get_option('pl_id');
 #var_dump($TWIG['videresendingsskjema']);
+
+#phpinfo();
