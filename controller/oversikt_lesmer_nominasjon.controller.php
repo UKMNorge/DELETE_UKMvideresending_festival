ec789ac4dd881d46788ba5369872bbd0884a841d
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2017-04-05 11:41:48 +0000

    Lagt til konferansier-nomineringsinformasjon.

--- controller/oversikt_lesmer_nominasjon.controller.php
+++ controller/oversikt_lesmer_nominasjon.controller.php
@@ -6,3 +6,4 @@ $TWIG['frister'] = get_site_option('UKMFvideresending_nominasjon_frister');
 $season = ($month > 7) ? date('Y')+1 : date('Y');
 $TWIG['skjema_ua'] = get_site_option('UKMFvideresending_nominasjon_ua_'.$season);
 $TWIG['skjema_ukmmedia'] = get_site_option('UKMFvideresending_nominasjon_ukmmedia_'.$season);
+$TWIG['skjema_konf'] = get_site_option('UKMFvideresending_nominasjon_konf_'.$season);
